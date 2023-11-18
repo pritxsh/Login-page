a91dfc2465839676f99f6336bb181de27ebcde84
Pritesh Vardekar <priteshv.264@gmail.com>
2023-11-18 18:42:23 +0530

    first version1.0 done

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let user = document.getElementById("text");
+let pass = document.getElementById("password");
+let flag = 1;
+function validateform() {
+
+    if (user.value == "") {
+        document.getElementById("usererror").innerHTML = "User Name is empty";
+        flag = 0;
+    }
+    else if (user.value.length < 3) {
+        document.getElementById("usererror").innerHTML = "User Name requires minimum 3 characters";
+        flag = 0;
+    } else {
+        document.getElementById("usererror").innerHTML = "";
+        flag = 1;
+    }
+
+    if (pass.value == "") {
+        document.getElementById("passerror").innerHTML = "Password is empty";
+        flag = 0;
+    }
+    else {
+        document.getElementById("passerror").innerHTML = "";
+        flag = 1;
+    }
+
+    if (flag) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
