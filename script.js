c5c687c8a8a2b0272270d3cc21116f6a49eead05
Pritesh Vardekar <priteshv.264@gmail.com>
2023-11-18 23:37:16 +0530

    changed logic for password validation

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 let user = document.getElementById("text");
 let pass = document.getElementById("password");
-let flag = 1;
+let userflag = 1;
+let passflag = 1;
 function validateform() {
 
     if (user.value == "") {
         document.getElementById("usererror").innerHTML = "User Name is empty";
-        flag = 0;
+        userflag = 0;
     }
     else if (user.value.length < 3) {
         document.getElementById("usererror").innerHTML = "User Name requires minimum 3 characters";
-        flag = 0;
+        userflag = 0;
     } else {
         document.getElementById("usererror").innerHTML = "";
-        flag = 1;
+        userflag = 1;
     }
 
 
     if (pass.value == "") {
         document.getElementById("passerror").innerHTML = "Password is empty";
-        flag = 0;
+        passflag = 0;
     }
 
     else {
         document.getElementById("passerror").innerHTML = "";
-        flag = 1;
+        passflag = 1;
     }
 
-    if (flag) {
+    if (userflag && passflag) {
         return true;
     }
     else {
         return false;
     }
+
 }
 
